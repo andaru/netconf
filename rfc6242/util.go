56837996aa054a84d20cd6964a6cd629effc830d
Andrew Fort <afort@choqolat.org>
2018-03-11 17:59:18 -0700

    rfc6242: add utility function SetChunkedFraming
    SetChunkedFraming is used to enable chunked framing mode on the
    objects passed to the function. Only *Decoder and *Encoder objects are
    subject to these changes.

---
+++ rfc6242/util.go
@@ -0,0 +1,14 @@
+package rfc6242
+
+// SetChunkedFraming enables chunked framing mode on all
+// *Decoder and *Encoder objects passed.
+func SetChunkedFraming(objects ...interface{}) {
+	for _, obj := range objects {
+		switch obj := obj.(type) {
+		case *Decoder:
+			obj.Framer = DecoderChunked
+		case *Encoder:
+			obj.ChunkedFraming = true
+		}
+	}
+}
