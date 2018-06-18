4e208e5b88a642fd131db503529877e7b2e3cb51
Andrew Fort <afort@choqolat.org>
2018-06-18 13:06:45 -0700

    ncerr: add more error wrappers

--- ncerr/option.go
+++ ncerr/option.go
@@ -1,7 +1,13 @@
 package ncerr
 
+import "fmt"
+
 // Option is an Error option function
 type Option func(*Error)
 
+func WithError(err error) Option    { return func(e *Error) { e.Message = err.Error() } }
 func WithMessage(msg string) Option { return func(e *Error) { e.Message = msg } }
-func WithType(t Type) Option        { return func(e *Error) { e.Type = t } }
+func WithMessageF(format string, args ...interface{}) Option {
+	return func(e *Error) { e.Message = fmt.Sprintf(format, args...) }
+}
+func WithType(t Type) Option { return func(e *Error) { e.Type = t } }
