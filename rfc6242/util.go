d5127eaa6dee14e0d5115c8801a57f53cd9a2199
Andrew Fort <afort@choqolat.org>
2018-06-18 19:13:14 -0700

    rfc6242: Add ClearChunkedFraming function
    Add the corresponding function to SetChunkedFraming. This need not be
    used on an io object used only for a single NETCONF transport.

--- rfc6242/util.go
+++ rfc6242/util.go
@@ -19,3 +19,20 @@ func SetChunkedFraming(objects ...interface{}) {
 		}
 	}
 }
+
+// ClearChunkedFraming disables chunked framing mode on any non-nil
+// *Decoder and *Encoder objects passed to it.
+func ClearChunkedFraming(objects ...interface{}) {
+	for _, obj := range objects {
+		switch obj := obj.(type) {
+		case *Decoder:
+			if obj != nil {
+				obj.framer = decoderEndOfMessage
+			}
+		case *Encoder:
+			if obj != nil {
+				obj.ChunkedFraming = false
+			}
+		}
+	}
+}
