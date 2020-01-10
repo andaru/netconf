2202c65e1cad8faa24463d886864a2619515f7a3
Andrew Fort <afort@choqolat.org>
2020-01-09 23:12:06 -0800

    Framing packageProvide "framing" package with RFC6242 transportframe format decoders for the two formats:  - end-of-message  - chunked framingAs many edge case tests as I could consider, allcode paths to all error cases are tested.

---
+++ framing/framing.go
@@ -0,0 +1,142 @@
+package framing
+
+import (
+	"bytes"
+	"io"
+	"strconv"
+
+	"bufio"
+
+	"github.com/pkg/errors"
+)
+
+// SplitEOM returns a bufio.SplitFunc suitable for RFC6242
+// "end-of-message delimited" NETCONF transport streams.
+//
+// It must only be used with bufio.Scanner who have a buffer of
+// at least 6 bytes (rarely is this a concern).
+func SplitEOM() bufio.SplitFunc {
+	var eofOK, seen bool
+	return func(b []byte, atEOF bool) (advance int, token []byte, err error) {
+		if atEOF && len(b) == 0 && !eofOK && seen {
+			err = io.ErrUnexpectedEOF
+			return
+		}
+		seen = seen || len(b) > 0
+		for cur := b[advance:]; advance < len(b); cur = b[advance:] {
+			if len(cur) < len(tokenEOM) && !atEOF {
+				return
+			}
+			switch idxeom := bytes.IndexByte(cur, ']'); {
+			case idxeom == -1:
+				advance += len(cur)
+				token = append(token, cur...)
+			case idxeom > 0:
+				advance += idxeom
+				token = append(token, cur[:idxeom]...)
+			case idxeom == 0:
+				advance++
+				for i := 1; i < len(tokenEOM); advance, i = advance+1, i+1 {
+					if cur[i] != tokenEOM[i] {
+						token = append(token, cur[:i]...)
+						return
+					}
+				}
+			}
+		}
+		eofOK = bytes.HasSuffix(b, tokenEOM)
+		return
+	}
+}
+
+// SplitChunked returns a bufio.SplitFunc suitable for decoding
+// "chunked framing" NETCONF transport streams.
+//
+// It must only be used with bufio.Scanner who have a buffer of
+// at least 16 bytes (rarely is this a concern).
+func SplitChunked() bufio.SplitFunc {
+	type stateT int
+	const (
+		headerStart stateT = iota
+		headerSize
+		data
+		endOfChunks
+	)
+	var state stateT
+	var cs, dataleft int
+
+	return func(b []byte, atEOF bool) (advance int, token []byte, err error) {
+		for cur := b[advance:]; err == nil && advance < len(b); cur = b[advance:] {
+			if len(cur) < 14 && !atEOF {
+				return
+			}
+			switch state {
+			case headerStart:
+				switch {
+				case bytes.HasPrefix(cur, []byte("\n#")):
+					if len(cur) < 4 {
+						err = ErrBadChunk
+						return
+					}
+					switch r := cur[2]; {
+					case r == '#':
+						advance += 3
+						state = endOfChunks
+					case r >= '1' && r <= '9':
+						advance += 2
+						state = headerSize
+					default:
+						err = ErrBadChunk
+					}
+				default:
+					err = ErrBadChunk
+				}
+			case headerSize:
+				switch idx := bytes.IndexByte(cur, '\n'); {
+				case idx < 1, idx > 10:
+					err = ErrBadChunk
+				default:
+					csize := cur[:idx]
+					if csizeVal, csizeErr := strconv.ParseUint(string(csize), 10, 31); csizeErr != nil {
+						err = ErrBadChunk
+					} else {
+						advance += idx + 1
+						dataleft = int(csizeVal)
+						state = data
+					}
+				}
+			case data:
+				var rsize int
+				if rsize = len(cur); dataleft < rsize {
+					rsize = dataleft
+				}
+				token = append(token, cur[:rsize]...)
+				advance += rsize
+				if dataleft -= rsize; dataleft < 1 {
+					state = headerStart
+					cs++
+				}
+				if rsize > 0 {
+					return
+				}
+			case endOfChunks:
+				switch r := cur[0]; {
+				case r == '\n' && cs > 0:
+					advance++
+					state = headerStart
+				default:
+					err = ErrBadChunk
+				}
+			}
+		}
+		if atEOF && dataleft > 0 || state != headerStart {
+			err = io.ErrUnexpectedEOF
+		}
+		return
+	}
+}
+
+// ErrBadChunk indicates a chunked framing protocol error occurred
+var ErrBadChunk = errors.New("bad chunk")
+
+var tokenEOM = []byte("]]>]]>")
---
+++ framing/framing_test.go
@@ -0,0 +1,107 @@
+package framing
+
+import (
+	"fmt"
+	"testing"
+
+	"bufio"
+
+	"strings"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFramingEOM(t *testing.T) {
+	for _, tc := range []struct {
+		input  string
+		want   string
+		hasErr bool
+	}{
+		{input: "]]>]]>", want: ""},
+		{
+			input: "012345789012345789012345789012345789012345789012345789012345789]]>]]>012345789012345789012345789]]>]]>",
+			want:  "012345789012345789012345789012345789012345789012345789012345789012345789012345789012345789",
+		},
+		{input: "foo>]]>bar]]>]]>bazoopa]]>]]>", want: "foo>]]>barbazoopa"},
+		{input: "]]>]]>]]>]]>baz]]>]]>", want: "baz"},
+		{input: "]]>]]>]]>]]>]]>]]>", want: ""},
+		{input: "]]>]]>foo]]>]]>]]>]]>", want: "foo"},
+		{input: "]]>]]>foo\n]]>]]>]]>]]>bar]]>]]>]]>]]>]]>]]>]]>]]>]]>]]>]]>]]>]]>]]>]]>]]>", want: "foo\nbar"},
+		{input: "foo]]>]]>", want: "foo"},
+		{input: "foo]]>]]>bar]]>]]>bazoopa]]>]]>", want: "foobarbazoopa"},
+		{input: "foo", want: "foo", hasErr: true},
+		{input: "foo]]>]]>bar]]>]]>bazoopa", want: "foobarbazoopa", hasErr: true},
+		{input: "]]>]]>]]>]]>]]>]]>abcdefghijklmnop", want: "abcdefghijklmnop", hasErr: true},
+		{},
+	} {
+		minlen := len("]]>]]>")
+		max := 32
+		for bsize := minlen; bsize < (minlen+1)+max; bsize++ {
+			t.Run(fmt.Sprintf("%s/%d", tc.input, bsize), func(t *testing.T) {
+				ck := assert.New(t)
+				scanner := bufio.NewScanner(strings.NewReader(tc.input))
+				scanner.Buffer(make([]byte, bsize), bsize)
+				scanner.Split(SplitEOM())
+				var got string
+				for scanner.Scan() {
+					got += scanner.Text()
+				}
+				serr := scanner.Err()
+				ck.True(serr == nil && !tc.hasErr || serr != nil && tc.hasErr, "want an error only if hasErr true, got %v (hasErr %v)", serr, tc.hasErr)
+				ck.Equal(tc.want, got)
+			})
+		}
+	}
+}
+
+func TestFramingChunked(t *testing.T) {
+	for _, tc := range []struct {
+		input  string
+		want   string
+		hasErr bool
+	}{
+		// empty input (special case, no end-of-chunks required)
+		{input: "", want: "", hasErr: false},
+		// valid data
+		{input: "\n#1\na\n##\n", want: "a"},
+		{input: "\n#1\na\n#1\nb\n#1\nc\n##\n", want: "abc"},
+		{input: "\n#2\nab\n#2\ncd\n#2\nef\n##\n", want: "abcdef"},
+		{input: "\n#3\nfoo\n#4\nfood\n##\n", want: "foofood"},
+		{input: "\n#4\nabc\n\n#4\ndef\n\n##\n", want: "abc\ndef\n"},
+
+		// coverage of all error causes
+		{input: "\n##\n", want: "", hasErr: true},
+		{input: "foo]]>]]>bar]]>]]>baz", want: "", hasErr: true},
+		{input: "\n#03\nfoo\n##\n", hasErr: true},
+		{input: "\n#92147483648\nffffffff...", hasErr: true},
+		{input: "\n#9\n012", hasErr: true},
+		{input: "\n#\na\n##\n", hasErr: true},
+		{input: "\n#1a\na\n##\n", hasErr: true},
+		{input: "\n#1\na\n##", hasErr: true},
+		{input: "\n#1\na\n#\n ", hasErr: true},
+		{input: "\n#1\na\n#", hasErr: true},
+		{input: "\n#1\na\n##\n ", hasErr: true},
+		{input: "\n#9\n0123456789\n##\n", hasErr: true},
+	} {
+		minlen := 16
+		max := 32
+		for bsize := minlen; bsize < (minlen+1)+max; bsize++ {
+			t.Run(fmt.Sprintf("%s/%d", tc.input, bsize), func(t *testing.T) {
+				ck := assert.New(t)
+				rdr := strings.NewReader(tc.input)
+				scanner := bufio.NewScanner(rdr)
+				scanner.Buffer(make([]byte, bsize), bsize*2)
+				scanner.Split(SplitChunked())
+				var got string
+				for scanner.Scan() {
+					got += scanner.Text()
+				}
+				serr := scanner.Err()
+				ck.True(serr == nil && !tc.hasErr || serr != nil && tc.hasErr, "want an error only if hasErr true, got %v (hasErr %v)", serr, tc.hasErr)
+				if tc.want != "" {
+					ck.Equal(tc.want, got)
+				}
+			})
+		}
+	}
+}
