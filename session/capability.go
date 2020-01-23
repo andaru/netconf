7db4846988134602865ded7a24311582e4796025
Andrew Fort <afort@choqolat.org>
2020-01-22 16:39:07 -0800

    Compare capabilities without parameters

--- session/capability.go
+++ session/capability.go
@@ -1,10 +1,13 @@
 package session
 
+import "strings"
+
 // Capabilities is a slice of strings denoting NETCONF capability URIs
 type Capabilities []string
 
 // Has returns true if uri is in the capabilities set
 func (c Capabilities) Has(uri string) bool {
+	uri = strings.SplitAfterN(uri, "?", 2)[0]
 	for _, cap := range c {
 		if uri == cap {
 			return true
