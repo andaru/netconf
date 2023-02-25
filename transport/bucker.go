2fc8cf37b40979056b887db37a9aac623de8b742
Andrew Fort <afort@choqolat.org>
2023-02-25 13:39:24 +1000

    improve bucker documentation

--- transport/bucker.go
+++ transport/bucker.go
@@ -5,33 +5,34 @@ import (
 	"io"
 )
 
-// buckFilter is a simple switchable input splitter/filter
+// buckFilter is an io.Reader wrapper implementation
+// to assist in initial NETCONF session transport handling.
 //
-// By default, it splits input around ']' characters
-// to ease detection of :base:1.0 end-of-message markers.
-// When buckNot is enabled, it flushes any previously
-// split input before reading directly from the source.
+// Initially, it splits input around ']' characters to ease detection
+// of :base:1.0 end-of-message markers.
+//
+// When disabled is set, it flushes any previously
+// split input before reading directly from the source
 type buckFilter struct {
-	src     io.Reader
-	bucked  []byte
-	buckNot bool
+	src      io.Reader
+	bucked   []byte
+	disabled bool
 }
 
 func (bf *buckFilter) Read(p []byte) (n int, err error) {
 	if len(bf.bucked) > 0 {
 		n = copy(p, bf.bucked)
 		bf.bucked = bf.bucked[n:]
-		p = p[:n]
+		// p = p[:n]
 		return n, err
 	}
-	if n, err = bf.src.Read(p); bf.buckNot {
+	if n, err = bf.src.Read(p); bf.disabled {
 		return n, err
 	}
 	if idx := bytes.IndexByte(p, ']'); idx > -1 {
 		bf.bucked = append(bf.bucked, p[idx:n]...)
-		p = p[:idx]
+		// p = p[:idx]
 		n = idx
 	}
 	return n, err
 }
-
